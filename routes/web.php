3cc1d6ea6d0569be0ed7189ef0a93186904cf2e6
KelvinAdiyanto <KelvinAdiyanto28@gmail.com>
2024-12-06 11:34:51 +0700

    Rearranged route

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
 Route::get('registration', [AuthController::class, 'registration'])->name('register');
 Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
-Route::get('dashboard', [AuthController::class, 'dashboard']);
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
 Route::get('/catatan', [KeuanganController::class, 'catatan'])->name('keuangan.catatan');
 Route::get('/tabungan', [KeuanganController::class, 'tabungan'])->name('keuangan.tabungan');
